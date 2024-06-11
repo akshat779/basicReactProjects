b87aea1b6dc6a93df7edd27f4ce4956e44cb7e83
Akshat Sharma <akshatsharma779@gmail.com>
2024-06-11 11:11:21 +0530

    Todo project initialised

---
+++ todolist/todoList/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Todo from './components/todo/todo.jsx'
+
+
+function App(){
+  return(
+    <>
+    <Todo />
+    </>
+  );
+};
+export default App
---
+++ todolist/todoList/src/components/todo/todo.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+import React from "react";
+
+function Todo(){
+    return(
+        <>
+        <h1>Yoyo!</h1>
+        </>
+    )
+}
+
+export default Todo;
