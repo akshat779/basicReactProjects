d4e2febd2244dfda704b61f9c7fc1fdcbf9ec05f
Akshat Sharma <akshatsharma779@gmail.com>
2024-06-14 07:50:49 +0530

    Series of new Projects

---
+++ ecommerce/ecommerceFilter/src/sidebar/colors/colors.jsx
@@ -0,0 +1,18 @@
+import './colors.css'
+import Input from '../../components/Input';
+
+function Colors(){
+    return(
+        <div className="containercolor">
+        <h2 className="color-title">Colors</h2>
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        
+        </div>
+    );
+};
+
+
+export default Colors;
---
+++ ecommerce/ecommerceFilter/src/sidebar/price/price.jsx
@@ -0,0 +1,19 @@
+import './price.css';
+import Input from '../../components/Input';
+function Price(){
+    return(
+
+        <div className="ml">
+            <h2 className ="sidebar-price-title">Price</h2>
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+        
+        </div>
+    );
+};
+
+
+export default Price;
