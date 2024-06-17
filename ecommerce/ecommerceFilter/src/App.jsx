2aa4d6d91b7f5efdf0afd3f137c166c2a4b17702
Akshat Sharma <akshatsharma779@gmail.com>
2024-06-17 10:59:08 +0530

    Starting again

--- ecommerce/ecommerceFilter/src/App.jsx
+++ ecommerce/ecommerceFilter/src/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react'
-import Nav from './Navigation/Nav'
-import Products from './Products/products'
-import Recommended from './recommended/recommended'
-import './index.css'
-import data from './assets/data'
-import Sidebar from './sidebar/sidebar'
-function App() {
-  const[selectedCategory,setSelectedCategory] = useState(null);
-  const[query,setQuery] = useState('');
-  const handleInputChange = (e) => {
-    setQuery(e.target.value);
-  }
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-  const filteredItems = data.filter((item) =>
-  item.title.toLocaleLowerCase().includes(query.toLocaleLowerCase() !== -1));
-  
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-    <Sidebar />
-    <Nav />
-    <Recommended />
-    <Products />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- ecommerce/ecommerceFilter/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-function Button(){
-    return(
-        <>
-        </>
-    )
-}
-
-export default Button;
